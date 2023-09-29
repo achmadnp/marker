b8e68f47021e1c7551204432684f1514b30d6dc6
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-09-29 13:12:47 +0200

    update index this affects session redirection  and reads couple api calls

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { getTableData, getTableFields } from "@/api-lib/db/tables";
 import { Dashboard } from "@/page-components/Home/Dashboard";
 import Sidebar from "@/page-components/Sidebar/Sidebar";
 import { getSession } from "next-auth/react";
@@ -5,25 +6,47 @@ import Router from "next/router";
 import { useEffect } from "react";
 
 export default function Home(props) {
-  useEffect(() => {
-    if (!props?.session) {
-      Router.push("/login");
-    }
-  }, [props?.session]);
-
+  const data = props.data;
+  const fields = props.fields;
   return (
     <div className="flex min-h-screen flex-nowrap">
       <Sidebar />
-      <Dashboard />
+      <Dashboard data={data} fields={fields} />
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
+
+  // session checks
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+      },
+    };
+  }
+
+  const fields = await getTableFields();
+  const data = await getTableData({ limit: 3 });
+
+  let tableData;
+  let tableFields;
+
+  fields.map((field) => {
+    tableFields = field.tablefields;
+  });
+
+  data.map((data) => {
+    tableData = data.data;
+  });
+
   return {
     props: {
       session,
+      fields: JSON.parse(JSON.stringify(tableFields)),
+      data: JSON.parse(JSON.stringify(tableData)),
     },
   };
 }
