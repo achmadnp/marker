86373de1df22d13da54235205224cf6ab2ee41fe
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-09-29 11:20:12 +0200

    update register api

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,7 +1,5 @@
 import bcrypt from "bcrypt";
 import Users from "@/lib/model/UserModel";
-import Organization from "@/lib/model/OrganisationModel";
-import { randomIntFromIntervall } from "@/lib/util/random";
 
 export default async function handler(req, res) {
   const body = req.body;
@@ -12,36 +10,15 @@ export default async function handler(req, res) {
     return;
   }
 
-  // if org didnt exist
-  const org = await Organization.findOne({ orgUUID: body?.organization });
-
-  if (!org) {
-    console.log("Couldn't find org'");
-    res.status(200).json({ message: "Organization not found", err: true });
-    return;
-  }
-
-  // generate username
-  const uname = usernameGenerator(body?.fullName);
-
   const salt = await bcrypt.genSalt(10);
 
   const hashedPass = await bcrypt.hash(body?.password, salt);
   const user = new Users({
-    username: uname,
+    username: body.username,
     email: body.email,
     password: hashedPass,
     fullname: body.fullName,
   });
   await user.save();
-  res.status(200).json({ message: "Register success", uname: uname });
+  res.status(200).json({ message: "Register success", uname: user.username });
 }
-
-const usernameGenerator = (fullname) => {
-  const rndInt = randomIntFromIntervall(111, 999);
-
-  const fn = fullname.split(" ")[0].substring(0, 2);
-  const ln = fullname.split(" ").pop().substring(0, 2);
-
-  return `${fn}${ln}${rndInt}`;
-};
