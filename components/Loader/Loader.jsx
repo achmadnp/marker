f3118413634167b72e2110e1cb10b5fe9fdbcd08
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-09-29 11:23:03 +0200

    create loader component

---
+++ components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+export const CubeLoader = () => {
+  return (
+    <div className="w-full absolute top-[50%] left-[50%] banter-loader">
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="inline-block text-center">Retrieving...</div>
+    </div>
+  );
+};
