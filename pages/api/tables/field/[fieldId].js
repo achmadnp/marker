97a311bc74a047a9e862d45076a7f89c2a5f0679
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-10-02 19:10:52 +0200

    add table field's api function

---
+++ pages/api/tables/field/[fieldId].js
@@ -0,0 +1,50 @@
+import { deleteField, editHeader } from "@/api-lib/db/tables";
+import { ncOpts } from "@/api-lib/nc";
+import nextConnect from "next-connect";
+
+export default nextConnect(ncOpts)
+  .get(async (req, res) => {
+    res.status(400).json({ message: "unimplemented route" });
+  })
+  .post(async (req, res) => {})
+  .put(async (req, res) => {
+    const updated = await editHeader({
+      fieldId: req.body.fieldId,
+      hname: req.body.fieldName,
+    });
+
+    if (updated.length != 0) {
+      return res.status(200).json({
+        operation: "PUT",
+        affectedData: updated.length,
+        status: "success",
+        message: "",
+      });
+    } else {
+      const err = "data does not exists or access not allowed";
+      return res.status(400).json({
+        operation: "PUT",
+        status: "failed",
+        message: `operation failed: ${err}`,
+      });
+    }
+  })
+  .delete(async (req, res) => {
+    const deleted = await deleteField({ fieldId: req.body.fieldId });
+
+    if (deleted.length != 0) {
+      return res.status(200).json({
+        operation: "DELETE",
+        affectedData: deleted.length,
+        status: "success",
+        message: `${deleted.length} data has been removed`,
+      });
+    } else {
+      const err = "data does not exists or access not allowed";
+      return res.status(400).json({
+        operation: "PUT",
+        status: "failed",
+        message: `operation failed: ${err}`,
+      });
+    }
+  });
