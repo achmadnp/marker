f20b0b44388fe56f6831b0954d976beec358e367
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-10-11 17:53:57 +0200

    update field api get and put

--- pages/api/tables/field/index.js
+++ pages/api/tables/field/index.js
@@ -1,10 +1,26 @@
-import { deleteField, editHeader } from "@/api-lib/db/tables";
+import {
+  createColumn,
+  emptyFieldData,
+  getTableFields,
+} from "@/api-lib/db/tables";
 import { ncOpts } from "@/api-lib/nc";
 import nextConnect from "next-connect";
 
 export default nextConnect(ncOpts)
   .get(async (req, res) => {
-    res.status(400).json({ message: "unimplemented route" });
+    const fields = await getTableFields();
+    if (fields) {
+      return res.status(200).json({
+        operation: "GET",
+        status: "success",
+        message: "fields has been successfully fetched",
+        fields: fields,
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Error: data empty or not found" });
+    }
   })
   .post(async (req, res) => {
     const created = await createColumn({
@@ -17,12 +33,31 @@ export default nextConnect(ncOpts)
         operation: "POST",
         createdData: created,
         status: "success",
-        message: `${created.length} data has been created`,
+        message: `field ${created.colField._id} has been created`,
       });
     } else {
       const err = "data does not exists or access not allowed";
       return res.status(400).json({
         operation: "POST",
+        status: "failed",
+        message: `operation failed: ${err}`,
+      });
+    }
+  })
+  .put(async (req, res) => {
+    const empty = await emptyFieldData({ fieldId: req.body.fieldId });
+    console.log(`deleted, ${empty}`);
+    if (empty.length != 0) {
+      return res.status(200).json({
+        operation: "PUT",
+        affectedData: empty.length,
+        status: "success",
+        message: `${empty.length} data has been removed`,
+      });
+    } else {
+      const err = "data does not exists or process not allowed";
+      return res.status(400).json({
+        operation: "PUT",
         status: "failed",
         message: `operation failed: ${err}`,
       });
