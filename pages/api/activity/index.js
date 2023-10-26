3e6c5641b4b2dee2d2feb39c3765cf3d02ecf85c
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-10-26 14:28:50 +0200

    Modify Activity API

--- pages/api/activity/index.js
+++ pages/api/activity/index.js
@@ -1,18 +1,30 @@
+import { createNewRowdata } from "@/api-lib/db/activity";
 import { getTableData } from "@/api-lib/db/tables";
 import { ncOpts } from "@/api-lib/nc";
 import nextConnect from "next-connect";
 
-export default nextConnect(ncOpts).get(async (req, res) => {
-  const data = await getTableData({ limit: 20 });
+export default nextConnect(ncOpts)
+  .get(async (req, res) => {
+    const data = await getTableData({ limit: 20 });
 
-  if (data) {
-    return res.status(200).json({
-      operation: "GET",
-      status: "success",
-      message: "data has been successfully fetched",
-      data: data,
+    if (data) {
+      return res.status(200).json(data);
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Error: data empty or not found" });
+    }
+  })
+  .post(async (req, res) => {
+    const created = await createNewRowdata({
+      tableId: "64f63fbc2dcbb4e3ad750230",
+      columnKeys: req.body.fields,
     });
-  } else {
-    return res.status(400).json({ message: "Error: data empty or not found" });
-  }
-});
+
+    if (created) {
+      return res.status(200).json(created);
+    } else {
+      return res.status(401).json({ message: "Error: Failed to create Data" });
+    }
+  })
+  .put(async (req, res) => {});
