df8ede496a259e52a15e4ccf523e15a99abce4ca
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-12-14 15:53:03 +0100

    edit home prop variable

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,10 @@ import Sidebar from "@/page-components/Sidebar/Sidebar";
 import { getSession } from "next-auth/react";
 
 export default function Home(props) {
-  const data = props.data;
-  const fields = props.fields;
   return (
     <div className="flex min-h-screen flex-nowrap">
       <Sidebar />
-      <Dashboard data={data} fields={fields} />
+      <Dashboard session={props.session} />
     </div>
   );
 }
