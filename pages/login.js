b415fa2c7b3a206643668da0be3b9ee6eb91f441
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2023-12-14 15:51:58 +0100

    fix session on login page

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,26 @@
 import { Login } from "@/page-components/Auth/Login";
+import { getSession } from "next-auth/react";
 
 const LoginPage = () => {
   return <Login />;
 };
 
 export default LoginPage;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
